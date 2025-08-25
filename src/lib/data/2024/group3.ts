3190d470ba29eb8861ceee6c3bf9b394b1a514f9
Elmir Yunusov <eyunusov@gmail.com>
2025-08-25 10:11:34 +0000

    Bir evvel duzelis etdiyin sehf uni ad olaraq hamisini yoxla Azerbaycan m

--- src/lib/data/2024/group3.ts
+++ src/lib/data/2024/group3.ts
@@ -5,17 +5,4 @@ import type { Specialty } from '../../types';
 // sub-3-tc: Tarix-Coğrafiya
 
 export const specialties: Specialty[] = [
-    // III Qrup - BDU
-    { id: 'spec-bdu-3-1', year: 2024, name: 'Azərbaycan dili və ədəbiyyatı müəllimliyi', universityId: 'uni-bdu', groupId: 'grp-3', subgroupId: 'sub-3-dt', level: 'bachelor', score: 539.1, educationForm: 'əyani', educationLanguage: 'az', paymentType: 'dövlət sifarişli', planCount: 0 },
-    { id: 'spec-bdu-3-2', year: 2024, name: 'Korreksiyaedici təlim', universityId: 'uni-bdu', groupId: 'grp-3', subgroupId: 'sub-3-dt', level: 'bachelor', score: 417.9, educationForm: 'əyani', educationLanguage: 'az', paymentType: 'dövlət sifarişli', planCount: 0 },
-    { id: 'spec-bdu-3-3', year: 2024, name: 'Tarix müəllimliyi', universityId: 'uni-bdu', groupId: 'grp-3', subgroupId: 'sub-3-tc', level: 'bachelor', score: 497.2, educationForm: 'əyani', educationLanguage: 'az', paymentType: 'dövlət sifarişli', planCount: 0 },
-    { id: 'spec-bdu-3-4', year: 2024, name: 'Təhsildə sosial-psixoloji xidmət', universityId: 'uni-bdu', groupId: 'grp-3', subgroupId: 'sub-3-dt', level: 'bachelor', score: 460.1, educationForm: 'əyani', educationLanguage: 'az', paymentType: 'dövlət sifarişli', planCount: 0 },
-    { id: 'spec-bdu-3-5', year: 2024, name: 'Beynəlxalq münasibətlər', universityId: 'uni-bdu', groupId: 'grp-3', subgroupId: 'sub-3-tc', level: 'bachelor', score: 376.5, educationForm: 'əyani', educationLanguage: 'az', paymentType: 'dövlət sifarişli', planCount: 0 },
-    { id: 'spec-bdu-3-6', year: 2024, name: 'Beynəlxalq münasibətlər (tədris ingilis dilində)', universityId: 'uni-bdu', groupId: 'grp-3', subgroupId: 'sub-3-tc', level: 'bachelor', score: 481.5, educationForm: 'əyani', educationLanguage: 'en', paymentType: 'dövlət sifarişli', planCount: 0 },
-    { id: 'spec-bdu-3-7', year: 2024, name: 'Dövlət və ictimai münasibətlər', universityId: 'uni-bdu', groupId: 'grp-3', subgroupId: 'sub-3-tc', level: 'bachelor', score: 536.4, educationForm: 'əyani', educationLanguage: 'az', paymentType: 'dövlət sifarişli', planCount: 0 },
-    { id: 'spec-bdu-3-8', year: 2024, name: 'Dövlət və ictimai münasibətlər', universityId: 'uni-bdu', groupId: 'grp-3', subgroupId: 'sub-3-tc', level: 'bachelor', score: 443.8, educationForm: 'qiyabi', educationLanguage: 'az', paymentType: 'dövlət sifarişli', planCount: 0 },
-    { id: 'spec-bdu-3-9', year: 2024, name: 'Dövlət və ictimai münasibətlər (tədris ingilis dilində)', universityId: 'uni-bdu', groupId: 'grp-3', subgroupId: 'sub-3-tc', level: 'bachelor', score: 582.3, educationForm: 'əyani', educationLanguage: 'en', paymentType: 'dövlət sifarişli', planCount: 0 },
-    { id: 'spec-bdu-3-10', year: 2024, name: 'Fəlsəfə', universityId: 'uni-bdu', groupId: 'grp-3', subgroupId: 'sub-3-dt', level: 'bachelor', score: 242.9, educationForm: 'əyani', educationLanguage: 'az', paymentType: 'dövlət sifarişli', planCount: 0 },
-    { id: 'spec-bdu-3-11', year: 2024, name: 'Filologiya (Azərbaycan dili və ədəbiyyatı)', universityId: 'uni-bdu', groupId: 'grp-3', subgroupId: 'sub-3-dt', level: 'bachelor', score: 413.0, educationForm: 'əyani', educationLanguage: 'az', paymentType: 'dövlət sifarişli', planCount: 0 },
-    { id: 'spec-bdu-3-12', year: 2024, name: 'Filologiya (Azərbaycan dili və ədəbiyyatı)', universityId: 'uni-bdu', groupId: 'grp-3', subgroupId: 'sub-3-dt', level: 'bachelor', score: 344.1, educationForm: 'qiyabi', educationLanguage: 'az', paymentType: 'dövlət sifarişli', planCount: 0 }
 ];
--- src/lib/data/2024/group5.ts
+++ src/lib/data/2024/group5.ts
@@ -1,11 +1,4 @@
 import type { Specialty } from '../../types';
 
 export const specialties: Specialty[] = [
-    // V Qrup - BDU
-    { id: 'spec-bdu-5-1', year: 2024, name: 'Fiziki tərbiyə və çağınşaqədərki hazırlıq müəllimliyi (Qazax filialı)', universityId: 'uni-bdu', groupId: 'grp-5', level: 'bachelor', score: 126.8, educationForm: 'əyani', educationLanguage: 'az', paymentType: 'dövlət sifarişli', planCount: 0 },
-    
-    // V Qrup - AZMİU
-    { id: 'spec-azmiu-5-1', year: 2024, name: 'Dizayn (interyer dizayn, qrafik dizayn)', universityId: 'uni-azmiu', groupId: 'grp-5', level: 'bachelor', score: 126.6, educationForm: 'əyani', educationLanguage: 'az', paymentType: 'dövlət sifarişli', planCount: 0 },
-    { id: 'spec-azmiu-5-1-sub', year: 2024, name: 'Dizayn (interyer dizayn, qrafik dizayn)', universityId: 'uni-azmiu', groupId: 'grp-5', level: 'college', score: 92.4, educationForm: 'əyani', educationLanguage: 'az', paymentType: 'dövlət sifarişli', planCount: 0 },
-    { id: 'spec-azmiu-5-2', year: 2024, name: 'Dizayn (interyer dizayn, qrafik dizayn, tədris ingilis dilində)', universityId: 'uni-azmiu', groupId: 'grp-5', level: 'bachelor', score: 198.7, educationForm: 'əyani', educationLanguage: 'en', paymentType: 'dövlət sifarişli', planCount: 0 }
 ];
