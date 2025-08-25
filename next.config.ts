861a4dab9f21b16c83a03213a1eac26c3dd09c0f
tanat22 <165506303+tanat22@users.noreply.github.com>
2025-08-25 18:11:34 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
   typescript: {
     ignoreBuildErrors: true,
@@ -10,6 +9,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,   // GitHub Pages-də mütləqdir!
     remotePatterns: [
       {
         protocol: 'https',
