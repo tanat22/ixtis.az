7aa9bf0ab6b404efab6b1783a4239d20cc3cbf36
Elmir Yunusov <eyunusov@gmail.com>
2025-08-25 12:44:07 +0000

    arxa fonu daha aciq rengde edek mence, qaramsaldir indi sanki

--- src/lib/data/university-info.ts
+++ src/lib/data/university-info.ts
@@ -32,5 +32,85 @@ export const universityInfo: Record<string, UniversityInfo> = {
   'uni-bmu': {
     description: 'Bakı Mühəndislik Universiteti mühəndislik, iqtisadiyyat və memarlıq sahələrində müasir standartlara cavab verən kadrlar hazırlayır. Tədrisin əksəriyyəti ingilis dilində aparılır və beynəlxalq əməkdaşlığa xüsusi önəm verilir.',
     website: 'https://beu.edu.az/'
+  },
+  'uni-aztu': {
+    description: 'Azərbaycan Texniki Universiteti (AzTU) ölkənin texniki və texnoloji sahələr üzrə aparıcı ali təhsil müəssisələrindən biridir. Maşınqayırma, metallurgiya, informatika və energetika kimi sahələrdə mühəndis hazırlığı həyata keçirir.',
+    website: 'https://aztu.edu.az/'
+  },
+  'uni-dia': {
+    description: 'Azərbaycan Respublikası Prezidenti yanında Dövlət İdarəçilik Akademiyası dövlət qulluğu üçün yüksək ixtisaslı menecerlər və idarəçilər hazırlayan ixtisaslaşmış ali təhsil müəssisəsidir.',
+    website: 'https://dia.edu.az/'
+  },
+  'uni-banm': {
+    description: 'Bakı Ali Neft Məktəbi neft-qaz mühəndisliyi və kimya mühəndisliyi sahələrində Böyük Britaniyanın Heriot-Vatt Universiteti ilə birgə beynəlxalq standartlara uyğun, tamamilə ingilis dilində tədris aparan müasir ali təhsil ocağıdır.',
+    website: 'https://www.bhos.edu.az/'
+  },
+  'uni-adu': {
+    description: 'Azərbaycan Dillər Universiteti dilçilik, tərcümə və beynəlxalq münasibətlər sahəsində ixtisaslaşmışdır. Bir çox xarici dilin tədrisi və filologiyası üzrə güclü ənənələrə malikdir.',
+    website: 'https://adu.edu.az/'
+  },
+  'uni-bsu': {
+    description: 'Bakı Slavyan Universiteti humanitar sahədə, xüsusilə rus dili və ədəbiyyatı, həmçinin digər slavyan və Avropa dilləri üzrə mütəxəssis hazırlığı ilə tanınır.',
+    website: 'https://bsu.edu.az/'
+  },
+  'uni-amu': {
+    description: 'Azərbaycan Tibb Universiteti ölkənin səhiyyə sistemi üçün həkim, stomatoloq, əczaçı və digər tibb mütəxəssisləri hazırlayan fundamental və ən böyük ali təhsil mərkəzidir.',
+    website: 'https://amu.edu.az/'
+  },
+  'uni-adim': {
+    description: 'Azərbaycan Dövlət Mədəniyyət və İncəsənət Universiteti aktyor, rejissor, sənətşünas, dizayner və musiqiçi kimi yaradıcı sahələr üzrə peşəkar kadrlar hazırlayır.',
+    website: 'https://admiu.edu.az/'
+  },
+  'uni-adra': {
+    description: 'Azərbaycan Dövlət Rəssamlıq Akademiyası rəngkarlıq, heykəltəraşlıq, dizayn və memarlıq kimi təsviri sənət sahələri üzrə ali təhsilli mütəxəssislər yetişdirir.',
+    website: 'https://www.azra.edu.az/'
+  },
+  'uni-atmu': {
+    description: 'Azərbaycan Turizm və Menecment Universiteti turizm, otelçilik, marketinq və menecment sahələri üzrə ixtisaslaşmışdır və bu sahədə ölkənin aparıcı ali məktəbidir.',
+    website: 'https://atmu.edu.az/'
+  },
+  'uni-sdu': {
+    description: 'Sumqayıt Dövlət Universiteti Sumqayıt və ətraf regionlar üçün müxtəlif sahələrdə, o cümlədən mühəndislik, iqtisadiyyat və pedaqogika üzrə kadrlar hazırlayan əsas ali təhsil mərkəzidir.',
+    website: 'https://sdu.edu.az/'
+  },
+  'uni-gdu': {
+    description: 'Gəncə Dövlət Universiteti Azərbaycanın qərb bölgəsinin ən böyük elm və təhsil mərkəzidir. Pedaqoji, humanitar və təbiət elmləri üzrə geniş spektrdə ixtisaslar təklif edir.',
+    website: 'https://gsu.az/'
+  },
+  'uni-ldu': {
+    description: 'Lənkəran Dövlət Universiteti ölkənin cənub regionu üçün aqrar elmlər, iqtisadiyyat, turizm və humanitar sahələr üzrə mütəxəssislər hazırlayan mühüm regional universitetdir.',
+    website: 'https://lsu.edu.az/'
+  },
+  'uni-qku': {
+    description: 'Qərbi Kaspi Universiteti (Western Caspian University) özəl ali təhsil müəssisəsi olaraq geniş spektrdə, o cümlədən iqtisadiyyat, politologiya, dizayn və informasiya texnologiyaları üzrə təhsil proqramları təklif edir.',
+    website: 'https://wcu.edu.az/'
+  },
+  'uni-xu': {
+    description: 'Xəzər Universiteti Azərbaycanın ilk özəl universitetlərindən biridir və əsasən ingilis dilində tədris aparır. Yüksək təhsil standartları və beynəlxalq mühiti ilə tanınır.',
+    website: 'https://khazar.org/'
+  },
+  'uni-aku': {
+    description: 'Azərbaycan Kooperasiya Universiteti əsasən iqtisadiyyat, ticarət, gömrük işi və ekspertiza sahələri üzrə mütəxəssis hazırlığı həyata keçirən ali təhsil müəssisəsidir.',
+    website: 'https://aku.edu.az/'
+  },
+  'uni-au': {
+    description: '“Azərbaycan” Universiteti tərcümə, filologiya, sosial iş və iqtisadiyyat kimi humanitar və sosial elmlər üzrə ixtisaslaşmış özəl ali təhsil müəssisəsidir.',
+    website: 'https://au.edu.az/'
+  },
+  'uni-maa': {
+    description: 'Milli Aviasiya Akademiyası mülki aviasiya sahəsi üçün pilotlar, mühəndislər və hava nəqliyyatının idarə edilməsi üzrə digər yüksək ixtisaslı mütəxəssislər hazırlayır.',
+    website: 'https://naa.edu.az/'
+  },
+  'uni-adau': {
+    description: 'Azərbaycan Dövlət Aqrar Universiteti kənd təsərrüfatının bütün sahələri – aqronomluq, zoomühəndislik, baytarlıq, aqromühəndislik üzrə ölkənin əsas ali təhsil və elm mərkəzidir.',
+    website: 'https://adau.edu.az/'
+  },
+  'uni-qu': {
+    description: 'Qarabağ Universiteti Azərbaycanın işğaldan azad edilmiş ərazilərində, Xankəndi şəhərində yenidən fəaliyyətə başlamışdır. Universitetin məqsədi regionun sosial-iqtisadi inkişafına təkan vermək üçün müxtəlif sahələr üzrə yüksək ixtisaslı kadrlar hazırlamaqdır.',
+    website: 'https://karabakh.edu.az/'
+  },
+  'uni-bbu': {
+    description: 'Bakı Biznes Universiteti biznesin idarə edilməsi, maliyyə və iqtisadiyyatın digər sahələri üzrə kadrlar hazırlayan ixtisaslaşmış özəl ali təhsil müəssisəsidir.',
+    website: 'https://bbu.edu.az/'
   }
 };
